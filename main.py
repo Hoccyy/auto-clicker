64ceb3f4ad2a6ed370c807024e6a6c615300780b
Hoccyy <94509399+Hoccyy@users.noreply.github.com>
2023-05-21 15:00:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication, QLabel, QMessageBox, QPushButton, \
+from PyQt5.QtWidgets  import QApplication, QLabel, QMessageBox, QPushButton, \
     QVBoxLayout, QWidget, QFileDialog, QGridLayout, QMessageBox, QMenuBar, QMenu, \
     QMainWindow
 
-from PyQt5.QtGui import QPixmap
-from PyQt5 import QtGui, QtCore
-from PyQt5.QtGui import QCursor
+from PyQt5.QtGui  import QPixmap
+from PyQt5        import QtGui, QtCore
+from PyQt5.QtGui  import QCursor
 
-app = QApplication (sys.argv)
+app     = QApplication (sys.argv)
 
-window = QWidget()
-grid = QGridLayout ()
-window.setLayout (grid)
+window  = QWidget()
+grid    = QGridLayout()
+window.setLayout(grid)
 
 widgets = {
     "Buttons" : [],
@@ -23,21 +23,20 @@ widgets = {
 class mainWindow:
     # Uses parameters to set window specs
     def __init__ (self, width, height, title, style, icon):
-        self.width = window.setFixedWidth (width)
+        self.width  = window.setFixedWidth (width)
         self.height = window.setFixedHeight (height)
-        self.title = window.setWindowTitle (title)
-        self.style = window.setStyleSheet (style)
-        self.icon = window.setWindowIcon (icon)
+        self.title  = window.setWindowTitle (title)
+        self.style  = window.setStyleSheet (style)
+        self.icon   = window.setWindowIcon (icon)
 
 class secondaryWindow (QMainWindow):
     # Uses parameters to set window specs
     def __init__ (self, width, height, title, style, icon):
-        self.width = window.setMinimumWidth (width)
+        self.width  = window.setMinimumWidth (width)
         self.height = window.setMinimumHeight (height)
-        self.title = window.setWindowTitle (title)
-        self.style = window.setStyleSheet (style)
-        self.icon = window.setWindowIcon (icon)
-
+        self.title  = window.setWindowTitle (title)
+        self.style  = window.setStyleSheet (style)
+        self.icon   = window.setWindowIcon (icon)
         QLabel('DOg')
     
     def createMenu(self):
@@ -51,17 +50,14 @@ class secondaryWindow (QMainWindow):
         #Horiz Line
         helpMenu.addSeparator()
 
-
-
-
 def frame0 ():
     # Creates the frame with the main window class
-    #                  (Wid.) (hei.)    Title             Styles              Icon
+    #                        (Wid.) (hei.)    Title             Styles               Icon
     frameStyles = "background-color:red;"
-    frame = mainWindow ((600), (600), "Auto Clicker", frameStyles, QtGui.QIcon ("ac.ico"))
+    frame       = mainWindow ((600), (600), "Auto Clicker", frameStyles, QtGui.QIcon ("ac.ico"))
 
 def frame1 ():
-    frame = secondaryWindow(600, 600, "Auto Clicker", "color: red;", QtGui.QIcon("ac.ico"))
+    frame       = secondaryWindow(600, 600, "Auto Clicker", "color: red;", QtGui.QIcon("ac.ico"))
 
 
 frame1()
